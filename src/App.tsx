ad800cb5ca425a7b39a924aae5ac5426eb134016
sebastianjuras <sebastian.juras@gmail.com>
2022-04-12 02:05:29 +0200

    Implemented Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom";
+
+import { LoginPageComponent as LoginPage } from './components/Login/LoginPage'
+import { DashboardComponent as Dashboard } from './components/Dashboard/Dashboard'
+import { ResetPasswordComponent as ResetPassword } from './components/ResetPassword/ResetPassword'
+import { RegisterComponent as Register} from './components/Register/Register'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route path="/" element={< LoginPage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<ResetPassword />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
     </div>
   );
 }
